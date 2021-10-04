19b87f060c61a7fa95f22bd4b1338aa3da9f5256
AugustoWalter <lucasassuncao2014@gmail.com>
2021-10-03 23:59:04 -0300

    heroku

--- src/main/java/br/com/med/clinica/MyConfig.java
+++ src/main/java/br/com/med/clinica/MyConfig.java
@@ -1,10 +1,20 @@
 package br.com.med.clinica;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@SpringBootApplication
+@EntityScan(basePackages = "br.com.med.clinica.administrativo.model")
+@ComponentScan(basePackages = { "br.*" })
+@EnableJpaRepositories(basePackages = { "br.com.med.clinica.atendimento.repository" })
+@EnableTransactionManagement
+@EnableWebMvc
 public class MyConfig implements WebMvcConfigurer { //talvez não seja necessario
 
 	@Override
